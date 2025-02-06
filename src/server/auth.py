54f7d075498e078fd3e9caf525c334d7a7b2fd88
Naught <77908661+Kn4ughty@users.noreply.github.com>
2025-02-07 10:23:55 +1100

    add seperate functions for post and get login methods

--- src/server/auth.py
+++ src/server/auth.py
@@ -1,11 +1,19 @@
+from flask import request
+
 # This is a circular import. However, flask offically reccomends it
 # https://flask.palletsprojects.com/en/stable/patterns/packages/
 from main import app
 
 
-@app.route("/login")
-def login_page():
+@app.get("/login")
+def login_get():
     with open("src/web/login/login.html") as file:
         d = file.read()
 
     return d
+
+
+@app.post("/login")
+def login_post():
+    s = f"u: {request.form['username']} \n p: {request.form['password']}"
+    return s
