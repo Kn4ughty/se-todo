c4da1f5f58d8c293710f288e956eed39bb4575a2
Naught <77908661+Kn4ughty@users.noreply.github.com>
2025-02-07 14:07:50 +1100

    add logging module

--- src/main.py
+++ src/main.py
@@ -1,8 +1,18 @@
+import logging as log
 from flask import Flask
+
 
 app = Flask(__name__)
 
 from server import auth  # noqa: E402
+
+# log.basicConfig(level=log.DEBUG)
+log.basicConfig(
+    level=log.INFO,
+    format="{asctime}-{levelname}: {message}",
+    style="{",
+    datefmt="%H:%M:%S",
+)
 
 
 @app.route("/")
