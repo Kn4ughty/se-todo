81ff66c5d6c917ff960fe8b067397be525a597e3
Knaughty <77908661+Kn4ughty@users.noreply.github.com>
2025-02-09 20:02:16 +1100

    switch to loguru for logging

--- src/main.py
+++ src/main.py
@@ -1,18 +1,9 @@
-import logging as log
+from loguru import logger
 from flask import Flask
-
 
 app = Flask(__name__)
 
 from server import auth  # noqa: E402
-
-# log.basicConfig(level=log.DEBUG)
-log.basicConfig(
-    level=log.INFO,
-    format="{asctime}-{levelname}: {message}",
-    style="{",
-    datefmt="%H:%M:%S",
-)
 
 
 @app.route("/")
