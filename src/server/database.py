d594911ab5e0975fd77289c7287ec0f65da4a911
Naught <77908661+Kn4ughty@users.noreply.github.com>
2025-02-09 18:44:56 +1100

    setup_data_directory stub

--- src/server/database.py
+++ src/server/database.py
@@ -1,13 +1,34 @@
 import sqlite3
+import logging as log
 from typing import List
 from pathlib import Path
+import os
 
 from server.types import User
 
 # This guide is good
 # https://www.geeksforgeeks.org/python-sqlite/
 
+
+local_share = Path("~/.local/share/").expanduser()
+data_dir_name = "naught"
+
 db_file_location = Path("~/.local/share/naught/todo/db.sqlite3")
+
+
+def setup_data_directory():
+    if not os.path.exists(local_share):
+        # ERROR
+        log.error(
+            f"{local_share} folder does not exist? This is strange. \
+            Not attempting to recover from this error, fix it manually"
+        )
+        raise FileExistsError
+
+    if not os.path.exists(os.path.join(local_share, data_dir_name)):
+        log.info(f"Creating {data_dir_name} folder in {local_share}")
+        os.mkdir(os.path.join(local_share, data_dir_name))
+
 
 sql_connection = sqlite3.connect(db_file_location)
 cur = sql_connection.cursor()
