048e80618d01f539baf9e583e07b8f95db1d550b
Naught <77908661+Kn4ughty@users.noreply.github.com>
2025-02-10 14:56:05 +1100

    add create_user_from_raw function to auth file

--- src/server/auth.py
+++ src/server/auth.py
@@ -1,16 +1,15 @@
 import bcrypt
 from flask import request
+from loguru import logger as log
 
 # This is a circular import. However, flask offically reccomends it
 # https://flask.palletsprojects.com/en/stable/patterns/packages/
 from main import app
 import server.database as database
+from server.types import User
 
 # Bcrypt guide
 # https://www.geeksforgeeks.org/hashing-passwords-in-python-with-bcrypt/
-
-
-print(database.get_users())
 
 
 @app.get("/login")
@@ -27,3 +26,14 @@ def login_post():
     password = request.form["password"]
 
     return username + " " + password
+
+
+def create_user_from_raw(username: str, password: str) -> User | str:
+    if len(username) >= 255:
+        log.info(f"Username input was longer than 255 characters: {username}")
+        return "Username must be shorter than 255 characters"
+
+    password_bytes = password.encode("utf-8")
+    salt = bcrypt.gensalt()
+    password_hash = bcrypt.hashpw(password_bytes, salt)
+    return User(username, password_hash)
