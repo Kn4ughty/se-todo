e29aec1fc3389069a7ebb94d4b8de9582ca5b46b
Knaughty <77908661+Kn4ughty@users.noreply.github.com>
2025-02-11 17:07:18 +1100

    switch login background image to be a pic of snow

--- src/main.py
+++ src/main.py
@@ -1,5 +1,4 @@
 from loguru import logger
-from flask import Flask, Response
 from flask import Flask, send_file
 
 app = Flask(__name__)
@@ -15,3 +14,8 @@ def index():
 @app.get("/index.css")
 def index_css():
     return send_file("web/index.css", mimetype="text/css")
+
+
+@app.get("/snow.JPEG")
+def snow():
+    return send_file("web/snow.JPEG", mimetype="image/jpeg")
