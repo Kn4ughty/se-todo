fd04c632734fa15eb0580aa1549b62975cf83070
Naught <77908661+Kn4ughty@users.noreply.github.com>
2025-02-11 11:53:43 +1100

    Update login screen to be pretty

--- src/main.py
+++ src/main.py
@@ -1,9 +1,9 @@
 from loguru import logger
-from flask import Flask
+from flask import Flask, Response
 
 app = Flask(__name__)
 
-from server import auth  # noqa: E402
+from server import auth, database  # noqa: E402
 
 
 @app.route("/")
@@ -12,3 +12,10 @@ def index():
         data = file.read()
 
     return data
+
+
+@app.get("/index.css")
+def index_css():
+    with open("src/web/index.css") as file:
+        data = file.read()
+    return Response(data, mimetype="text/css")
