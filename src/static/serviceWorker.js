f465044984626d3dedb077c29c289e8c1fd3e3ad
Naught <77908661+Kn4ughty@users.noreply.github.com>
2025-02-13 14:42:48 +1100

    Add precaching to serviceWorker

--- src/static/serviceWorker.js
+++ src/static/serviceWorker.js
@@ -1,11 +1,19 @@
 
+const cacheName = "MyCache_1";
+const precachedResources = ["/", "/index.css", "snow.JPEG"];
+
+async function precache() {
+    const cache = await caches.open(cacheName);
+    return cache.addAll(precachedResources);
+}
 
 self.addEventListener('install', function(event) {
- console.log('[Service Worker] Installing Service Worker ...', event);
+    console.log('[Service Worker] Installing Service Worker ...', event);
+    event.waitUntil(precache());
 });
 self.addEventListener('activate', function(event) {
- console.log('[Service Worker] Activating Service Worker ...', event);
+    console.log('[Service Worker] Activating Service Worker ...', event);
 });
 self.addEventListener('fetch', function(event) {
- console.log('[Service Worker] Fetching something ...', event);
+    console.log('[Service Worker] Fetching something ...', event);
 });
