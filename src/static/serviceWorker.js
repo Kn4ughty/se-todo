68398f39493508663e0bc99805c17ca2703149c0
Naught <77908661+Kn4ughty@users.noreply.github.com>
2025-02-14 09:02:51 +1100

    Add automatic caching of resources to service worker

--- src/static/serviceWorker.js
+++ src/static/serviceWorker.js
@@ -7,6 +7,24 @@ async function precache() {
     return cache.addAll(precachedResources);
 }
 
+async function cacheFirst(request) {
+    console.log(`caching: ${request}`)
+    const cachedResponse = await caches.match(request);
+    if (cachedResponse) {
+        return cachedResponse;
+    }
+    try {
+        const networkResponse = await fetch(request);
+        if (networkResponse.ok) {
+            const cache = await caches.open("MyCache_1");
+            cache.put(request, networkResponse.clone());
+        }
+        return networkResponse;
+    } catch (error) {
+        return Response.error();
+    }
+}
+
 self.addEventListener('install', function(event) {
     console.log('[Service Worker] Installing Service Worker ...', event);
     event.waitUntil(precache());
@@ -16,4 +34,7 @@ self.addEventListener('activate', function(event) {
 });
 self.addEventListener('fetch', function(event) {
     console.log('[Service Worker] Fetching something ...', event);
+    if (precachedResources.includes(url.pathname)) {
+        event.respondWith(cacheFirst(event.request));
+    }
 });
