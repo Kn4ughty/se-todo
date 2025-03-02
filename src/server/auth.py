dfce9170d08872cc963d25ef2b3109d63b06b774
Knaughty <77908661+Kn4ughty@users.noreply.github.com>
2025-03-02 19:00:31 +1100

    Fix signup to accept alphanumeric instead of just alpha

--- src/server/auth.py
+++ src/server/auth.py
@@ -108,8 +108,8 @@ def add_user():
 
     log.info(f"User being added via /signup. username: {username}")
 
-    if not username.replace("_", "").isalpha():
-        return jsonify("Non alpha username"), 400
+    if not username.replace("_", "").isalnum():
+        return jsonify("Non alpha numeric username"), 400
     if len(username) < 3:
         return jsonify("Username too short"), 400
 
